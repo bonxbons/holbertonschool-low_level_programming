aa8c4148ee3e5ccb6e94ce15b273244747cb8d5b
bonxbons <ivonnexlopez@gmail.com>
2023-10-01 20:15:28 -0700

    tennct

--- functions_nested_loops/10-main.c
+++ functions_nested_loops/10-main.c
@@ -1,20 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- *  main - check the code.
- *   
- *  Return: Always 0.
+ *   main - check the code
+ * 
+ *   Return: Always 0.
  */
 
 int main(void)
 {
-	    print_times_table(3);
-	    _putchar('\n');
-	    print_times_table(5);
-	    _putchar('\n');
-	    print_times_table(98);
-	    _putchar('\n');
-	    print_times_table(12);  
-	    return (0);
+	    int n;
+
+	        n = add(89, 9);
+		printf("%d\n", n);
+		return (0);
 
 }
