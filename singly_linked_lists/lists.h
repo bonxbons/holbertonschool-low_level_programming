f892d9fd7bd1c37ba10a6d7af9702ac4f4f0ee3c
root <ivonnexlopez@gmail.com>
2023-11-12 18:47:08 -0800

    hope this passes

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,12 +1,12 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>  // Include for size_t
+#include <stddef.h>  /* Include for size_t */
 
 /* Define the structure for a linked list node */
 typedef struct list_t {
-    char *str;         // Assuming str is a string (adjust data type as needed)
-    unsigned int len;  // Assuming len is the length of the string
+    char *str;               /* Assuming str is a string (adjust data type as needed) */
+    unsigned int len;        /* Assuming len is the length of the string */
     struct list_t *next;
 } list_t;
 
@@ -14,3 +14,4 @@ typedef struct list_t {
 size_t print_list(const list_t *h);
 
 #endif /* LISTS_H */
+
