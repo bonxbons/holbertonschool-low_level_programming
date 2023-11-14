a5e11087bc83bc1101f4babe1db61f46d6eb75dd
root <ivonnexlopez@gmail.com>
2023-11-13 16:11:23 -0800

    done?

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,3 +9,14 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[c++])
 		;
 
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = --c;
+	new_node->next = (*head);
+	(*head) = new_node;
+
+	return (*head);
+}
