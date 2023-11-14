efd8e9d98e08f3b2c006bb421ed590c8d6fdfecb
root <ivonnexlopez@gmail.com>
2023-11-13 16:16:11 -0800

    error

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,34 +2,26 @@
 #include "lists.h"
 
 /**
- * print_list - print all elements of a linked list.
+ * print_list - prints all the elements of a linked list.
  * @h: a linked list to print
  *
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	size_t counter = 0;
+    size_t counter = 0;
 
-<<<<<<< HEAD
     while (h != NULL)
     {
         if (h->str == NULL)
             printf("[0] (nil)\n");
         else
-            printf("[%u] %s\n", h-:       
-=======
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
+            printf("[%u] %s\n", h->len, h->str);
 
-		++counter;
-		h = h->next;
-	}
+        ++counter;
+        h = h->next;
+    }
 
-	return (counter);
+    return counter;
 }
->>>>>>> f27bc5cec3889be1bd232fafa88413d3f4aaff19
+
