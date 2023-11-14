144f905463faa74e15b8d351b07173d31e8a4142
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-11-13 18:03:50 -0600

    Betty style errors fixed 1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * list_len - count elements of a linked list.
+ * list_len - count the elements of a linked list.
  * @h: a singly linked list
  *
  * Return: the number of nodes
  */
 size_t list_len(const list_t *h)
 {
-    size_t counter = 0;
+	size_t counter = 0;
 
-    while (h != NULL)
-    {
-        ++counter;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		++counter;
+		h = h->next;
+	}
 
-    return (counter);
+	return (counter);
 }
-
