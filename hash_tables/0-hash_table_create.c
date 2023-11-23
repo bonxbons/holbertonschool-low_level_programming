765af14f768937978d9c20714fc122aebcc2657b
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-11-22 23:02:17 -0600

    fixing spacing bc betty mad

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,23 +10,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    unsigned long int i = 0;
-    hash_table_t *new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	unsigned long int i = 0;
+	hash_table_t *new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
-    if (new_table == NULL)
-        return (NULL);
+	if (new_table == NULL)
+		return (NULL);
 
-    new_table->size = size;
-    new_table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
+	new_table->size = size;
+	new_table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
 
-    if (new_table->array == NULL)
-    {
-        free(new_table);
-        return (NULL);
-    }
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
 
-    for (i = 0; i < size; i++)
-        new_table->array[i] = NULL;
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 
-    return (new_table);
+	return (new_table);
 }
