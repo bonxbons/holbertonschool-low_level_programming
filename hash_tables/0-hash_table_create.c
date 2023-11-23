141c4380ac0c1d2108695d9ef2d06404d784ecb3
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-11-22 22:43:39 -0600

    fixing my function and correctingf errors

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,29 +6,22 @@
  *
  * Return: A pointer to the newly created hash table, or NULL if an error occurred.
  */
-hash_table_t *hash_table_create(unsigned long int size)
-{
-    hash_table_t *htable = malloc(sizeof(hash_table_t));
+hash_table_t *htable = malloc(sizeof(hash_table_t));
 
-    if (htable == NULL)
-    {
+    if (!htable)
         return NULL;
-    }
 
     htable->size = size;
-
     htable->array = malloc(sizeof(hash_node_t *) * size);
-    if (htable->array == NULL)
+
+    if (!htable->array)
     {
         free(htable);
         return NULL;
     }
 
     for (unsigned long int i = 0; i < size; ++i)
-    {
         htable->array[i] = NULL;
-    }
 
     return htable;
 }
-
