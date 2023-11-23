3a44f5055eb84f587961d36683d4c3e39e193b10
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-11-22 23:03:20 -0600

    fixing spaces

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -11,9 +11,9 @@
  */
 typedef struct hash_node_s
 {
-    char *key;
-    char *value;
-    struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -26,10 +26,11 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-    unsigned long int size;
-    hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
+/* Function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
 
-#endif
+#endif /* HASH_TABLES_H */
