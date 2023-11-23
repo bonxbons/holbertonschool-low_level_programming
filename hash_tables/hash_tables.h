53acb2c1baf619fadb307ab1a9fa368f4673bb86
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-11-22 22:59:54 -0600

    updating with updated code

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -3,6 +3,7 @@
 
 /**
  * struct hash_node_s - Node of a hash table
+ *
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
@@ -17,6 +18,7 @@ typedef struct hash_node_s
 
 /**
  * struct hash_table_s - Hash table data structure
+ *
  * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
@@ -28,8 +30,6 @@ typedef struct hash_table_s
     hash_node_t **array;
 } hash_table_t;
 
-/* Function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
 
 #endif
-
