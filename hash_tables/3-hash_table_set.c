6015f14d30542b286323ec50856f0a0a7056ddb7
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-11-22 23:31:30 -0600

    fixing for betty style

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,11 +15,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node = NULL, *current_node = NULL;
 
-	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
-		return 0;
+	if (ht == NULL || key == NULL || key[0] == '\0')
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
 	current_node = ht->array[index];
 
 	while (current_node != NULL)
@@ -29,21 +28,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(current_node->value);
 			current_node->value = strdup(value);
 			if (current_node->value == NULL)
-				return 0;
-			return 1;
+				return (0);
+			return (1);
 		}
 		current_node = current_node->next;
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-		return 0;
+		return (0);
 
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
-		return 0;
+		return (0);
 	}
 
 	new_node->value = strdup(value);
@@ -51,12 +50,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		free(new_node->key);
 		free(new_node);
-		return 0;
+		return (0);
 	}
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
-	return 1;
+	return (1);
 }
-
