e2190f7299775e910e44f8f2ad08da94e53646e9
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-11-22 22:56:05 -0600

    remade my function because it wasnt working

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,27 +1,32 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
+ * hash_table_create - Creates a hash table
+ * @size: The size of the hash table
  *
- * Return: A pointer to the newly created hash table, or NULL if an error occurred.
+ * Return: The newly created hash table
  */
-hash_table_t *htable = malloc(sizeof(hash_table_t));
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    unsigned long int i = 0;
+    hash_table_t *new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
-    if (!htable)
-        return NULL;
+    if (new_table == NULL)
+        return (NULL);
 
-    htable->size = size;
-    htable->array = malloc(sizeof(hash_node_t *) * size);
+    new_table->size = size;
+    new_table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
 
-    if (!htable->array)
+    if (new_table->array == NULL)
     {
-        free(htable);
-        return NULL;
+        free(new_table);
+        return (NULL);
     }
 
-    for (unsigned long int i = 0; i < size; ++i)
-        htable->array[i] = NULL;
+    for (i = 0; i < size; i++)
+        new_table->array[i] = NULL;
 
-    return htable;
+    return (new_table);
 }
