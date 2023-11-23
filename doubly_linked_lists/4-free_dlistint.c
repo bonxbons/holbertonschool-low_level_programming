df754f0155a4e5a227e6fb90a5c99141e061501a
bonxbons <ivonnexlopez@gmail.com>
2023-11-22 18:45:50 -0800

    writing function to free the dlist

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - Frees a dlistint_t list.
+ * @head: A pointer to the head of the dlistint_t list.
+ *
+ * Description: Frees all nodes of a doubly linked list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *head, *next;
+
+	if (!head)
+		return;
+
+	first = head;
+	while (first)
+	{
+		next = first->next;
+		free(first);
+		first = next;
+	}
+}
+
