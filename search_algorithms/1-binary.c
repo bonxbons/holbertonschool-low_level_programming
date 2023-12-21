40d00889ae3f9ce24568fb61f8c5332bd7022715
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-21 14:59:49 -0600

    SPACES BAGUJS B

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -11,33 +11,33 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0, right = size - 1, mid, i;
+	size_t left = 0, right = size - 1, mid, i;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    while (left <= right)
-    {
-        printf("Searching in array: ");
-        for (i = left; i <= right; i++)
-        {
-            printf("%d", array[i]);
-            if (i < right)
-                printf(", ");
-            else
-                printf("\n");
-        }
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+			else
+				printf("\n");
+		}
 
-        mid = (left + right) / 2;
+		mid = (left + right) / 2;
 
-        if (array[mid] == value)
-            return (mid);
+		if (array[mid] == value)
+			return (mid);
 
-        if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
 
-    return (-1); /* Value not found */
+	return (-1); /* Value not found */
 }
